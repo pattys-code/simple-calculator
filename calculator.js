04def7d9434a100d1ab829e16d00abbe43236359
Patricia <162380840+pattys-code@users.noreply.github.com>
2026-05-15 19:45:24 +0200

    add event listeners to buttons, connect operator functions to event listener logic

--- calculator.js
+++ calculator.js
@@ -1,29 +1,76 @@
 const current = document.querySelector('.current');
-const history = document.querySelector('.history');
 const numberBtns = document.querySelectorAll('.number');
 const clearBtn = document.querySelector('.clear');
 const resultBtn = document.querySelector('.result');
-const addBtn = document.querySelector('.add');
-const subtractBtn = document.querySelector('.subtract');
-const multiplyBtn = document.querySelector('.multiply');
-const divideBtn = document.querySelector('.divide');
+const operatorBtns = document.querySelectorAll('.operator');
+let curr_number1 = '';
+let curr_number2 = '';
+let curr_operator = '';
 
 // operators
-function add (number1, number2) {
-    return number1 + number2;
+function add () {
+    return curr_number1 + curr_number2;
 }
-function subtract (number1, number2) {
-    return number1 - number2;
+function subtract () {
+    return curr_number1 - curr_number2;
 }
-function multiply (number1, number2) {
-    return number1 * number2;
+function multiply () {
+    return curr_number1 * curr_number2;
 }
-function divide (number1, number2) {
-    return number1 / number2;
+function divide () {
+    return curr_number1 / curr_number2;
 }
 
-// buttons
+function calculate () {
+    curr_number1 = Number(curr_number1);
+    curr_number2 =Number(curr_number2);
+    if (curr_operator==='+') {curr_number1=add()}
+    else if (curr_operator==='-') {curr_number1=subtract()}
+    else if (curr_operator==='x') {curr_number1=multiply()}
+    else if (curr_operator==='%') {curr_number1=divide()}
+    curr_number2 ='';
+    curr_operator ='';
+    current.textContent = curr_number1+curr_operator+curr_number2;
+}
+
+// numbers 
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        if (curr_operator==='') {
+            curr_number1 += button.textContent;
+        }
+        else if (curr_operator!=='') {
+            curr_number2 += +button.textContent;
+        }
+        current.textContent = curr_number1+curr_operator+curr_number2;
+        console.log(curr_number1);
+        console.log(curr_number2);
+        console.log(curr_operator);
+    })
+})
+
+//opeartors
+operatorBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        if (curr_number2!=='') {
+            calculate();
+        }
+        if (curr_number1!=='') {
+            curr_operator = button.textContent;
+            current.textContent = curr_number1+curr_operator+curr_number2;
+        }
+        
+        console.log(curr_number1);
+        console.log(curr_number2);
+        console.log(curr_operator);
+    })
+})
+
+resultBtn.addEventListener('click', ()=> {
+    calculate();
+    curr_number1 ='';
+})
 
 
 
-console.log(multiply(0.2,5));
+
